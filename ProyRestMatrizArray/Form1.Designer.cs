7e6bc46bb41d21094813f076ec3962f575e65211
cintiaxdiaz <63810271+cintiaxdiaz@users.noreply.github.com>
2020-04-16 18:52:18 -0400

    Add project files.

---
+++ ProyRestMatrizArray/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace ProyRestMatrizArray
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBoxUsuario = new System.Windows.Forms.TextBox();
+            this.textBoxPassUsuario = new System.Windows.Forms.TextBox();
+            this.buttonIngresar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxUsuario
+            // 
+            this.textBoxUsuario.BackColor = System.Drawing.SystemColors.Window;
+            this.textBoxUsuario.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxUsuario.Location = new System.Drawing.Point(478, 281);
+            this.textBoxUsuario.Multiline = true;
+            this.textBoxUsuario.Name = "textBoxUsuario";
+            this.textBoxUsuario.Size = new System.Drawing.Size(174, 39);
+            this.textBoxUsuario.TabIndex = 0;
+            this.textBoxUsuario.Text = "Usuario";
+            this.textBoxUsuario.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBoxPassUsuario
+            // 
+            this.textBoxPassUsuario.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPassUsuario.Location = new System.Drawing.Point(479, 335);
+            this.textBoxPassUsuario.Multiline = true;
+            this.textBoxPassUsuario.Name = "textBoxPassUsuario";
+            this.textBoxPassUsuario.Size = new System.Drawing.Size(173, 39);
+            this.textBoxPassUsuario.TabIndex = 1;
+            this.textBoxPassUsuario.Text = "Contraseña";
+            this.textBoxPassUsuario.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // buttonIngresar
+            // 
+            this.buttonIngresar.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonIngresar.Location = new System.Drawing.Point(479, 389);
+            this.buttonIngresar.Name = "buttonIngresar";
+            this.buttonIngresar.Size = new System.Drawing.Size(174, 39);
+            this.buttonIngresar.TabIndex = 2;
+            this.buttonIngresar.Text = "Ingresar";
+            this.buttonIngresar.UseVisualStyleBackColor = true;
+            this.buttonIngresar.Click += new System.EventHandler(this.buttonIngresar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Moccasin;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonIngresar);
+            this.Controls.Add(this.textBoxPassUsuario);
+            this.Controls.Add(this.textBoxUsuario);
+            this.Name = "Form1";
+            this.Text = "Acceso";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxUsuario;
+        private System.Windows.Forms.TextBox textBoxPassUsuario;
+        private System.Windows.Forms.Button buttonIngresar;
+    }
+}
+
---
+++ ProyRestMatrizArray/Form2.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace ProyRestMatrizArray
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.btnMesa1 = new System.Windows.Forms.Button();
+            this.btnMesa2 = new System.Windows.Forms.Button();
+            this.btnMesa3 = new System.Windows.Forms.Button();
+            this.btnMesa4 = new System.Windows.Forms.Button();
+            this.btnMesa5 = new System.Windows.Forms.Button();
+            this.btnMesa6 = new System.Windows.Forms.Button();
+            this.btnMesa7 = new System.Windows.Forms.Button();
+            this.btnMesa8 = new System.Windows.Forms.Button();
+            this.btnMesa9 = new System.Windows.Forms.Button();
+            this.btnMesa10 = new System.Windows.Forms.Button();
+            this.btnMesa11 = new System.Windows.Forms.Button();
+            this.btnMesa12 = new System.Windows.Forms.Button();
+            this.btnMesa13 = new System.Windows.Forms.Button();
+            this.btnMesa14 = new System.Windows.Forms.Button();
+            this.btnMesa15 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnMesa1
+            // 
+            this.btnMesa1.Font = new System.Drawing.Font("Calibri", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMesa1.Image = ((System.Drawing.Image)(resources.GetObject("btnMesa1.Image")));
+            this.btnMesa1.Location = new System.Drawing.Point(49, 24);
+            this.btnMesa1.Name = "btnMesa1";
+            this.btnMesa1.Size = new System.Drawing.Size(79, 80);
+            this.btnMesa1.TabIndex = 0;
+            this.btnMesa1.Text = "M 1";
+            this.btnMesa1.UseVisualStyleBackColor = true;
+            // 
+            // btnMesa2
+            // 
+            this.btnMesa2.Font = new System.Drawing.Font("Calibri", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMesa2.Image = ((System.Drawing.Image)(resources.GetObject("btnMesa2.Image")));
+            this.btnMesa2.Location = new System.Drawing.Point(163, 24);
+            this.btnMesa2.Name = "btnMesa2";
+            this.btnMesa2.Size = new System.Drawing.Size(79, 80);
+            this.btnMesa2.TabIndex = 1;
+            this.btnMesa2.Text = "M 2";
+            this.btnMesa2.UseVisualStyleBackColor = true;
+            // 
+            // btnMesa3
+            // 
+            this.btnMesa3.Font = new System.Drawing.Font("Calibri", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMesa3.Image = ((System.Drawing.Image)(resources.GetObject("btnMesa3.Image")));
+            this.btnMesa3.Location = new System.Drawing.Point(276, 24);
+            this.btnMesa3.Name = "btnMesa3";
+            this.btnMesa3.Size = new System.Drawing.Size(79, 80);
+            this.btnMesa3.TabIndex = 2;
+            this.btnMesa3.Text = "M 3";
+            this.btnMesa3.UseVisualStyleBackColor = true;
+            // 
+            // btnMesa4
+            // 
+            this.btnMesa4.Font = new System.Drawing.Font("Calibri", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMesa4.Image = ((System.Drawing.Image)(resources.GetObject("btnMesa4.Image")));
+            this.btnMesa4.Location = new System.Drawing.Point(390, 24);
+            this.btnMesa4.Name = "btnMesa4";
+            this.btnMesa4.Size = new System.Drawing.Size(79, 80);
+            this.btnMesa4.TabIndex = 3;
+            this.btnMesa4.Text = "M 4";
+            this.btnMesa4.UseVisualStyleBackColor = true;
+            // 
+            // btnMesa5
+            // 
+            this.btnMesa5.Font = new System.Drawing.Font("Calibri", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMesa5.Image = ((System.Drawing.Image)(resources.GetObject("btnMesa5.Image")));
+            this.btnMesa5.Location = new System.Drawing.Point(504, 24);
+            this.btnMesa5.Name = "btnMesa5";
+            this.btnMesa5.Size = new System.Drawing.Size(79, 80);
+            this.btnMesa5.TabIndex = 4;
+            this.btnMesa5.Text = "M 5";
+            this.btnMesa5.UseVisualStyleBackColor = true;
+            // 
+            // btnMesa6
+            // 
+            this.btnMesa6.Font = new System.Drawing.Font("Calibri", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMesa6.Image = ((System.Drawing.Image)(resources.GetObject("btnMesa6.Image")));
+            this.btnMesa6.Location = new System.Drawing.Point(49, 151);
+            this.btnMesa6.Name = "btnMesa6";
+            this.btnMesa6.Size = new System.Drawing.Size(79, 80);
+            this.btnMesa6.TabIndex = 5;
+            this.btnMesa6.Text = "M 6";
+            this.btnMesa6.UseVisualStyleBackColor = true;
+            // 
+            // btnMesa7
+            // 
+            this.btnMesa7.Font = new System.Drawing.Font("Calibri", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMesa7.Image = ((System.Drawing.Image)(resources.GetObject("btnMesa7.Image")));
+            this.btnMesa7.Location = new System.Drawing.Point(163, 151);
+            this.btnMesa7.Name = "btnMesa7";
+            this.btnMesa7.Size = new System.Drawing.Size(79, 80);
+            this.btnMesa7.TabIndex = 6;
+            this.btnMesa7.Text = "M 7";
+            this.btnMesa7.UseVisualStyleBackColor = true;
+            // 
+            // btnMesa8
+            // 
+            this.btnMesa8.Font = new System.Drawing.Font("Calibri", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMesa8.Image = ((System.Drawing.Image)(resources.GetObject("btnMesa8.Image")));
+            this.btnMesa8.Location = new System.Drawing.Point(276, 151);
+            this.btnMesa8.Name = "btnMesa8";
+            this.btnMesa8.Size = new System.Drawing.Size(79, 80);
+            this.btnMesa8.TabIndex = 7;
+            this.btnMesa8.Text = "M 8";
+            this.btnMesa8.UseVisualStyleBackColor = true;
+            // 
+            // btnMesa9
+            // 
+            this.btnMesa9.Font = new System.Drawing.Font("Calibri", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMesa9.Image = ((System.Drawing.Image)(resources.GetObject("btnMesa9.Image")));
+            this.btnMesa9.Location = new System.Drawing.Point(390, 151);
+            this.btnMesa9.Name = "btnMesa9";
+            this.btnMesa9.Size = new System.Drawing.Size(79, 80);
+            this.btnMesa9.TabIndex = 8;
+            this.btnMesa9.Text = "M 9";
+            this.btnMesa9.UseVisualStyleBackColor = true;
+            // 
+            // btnMesa10
+            // 
+            this.btnMesa10.Font = new System.Drawing.Font("Calibri", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMesa10.Image = ((System.Drawing.Image)(resources.GetObject("btnMesa10.Image")));
+            this.btnMesa10.Location = new System.Drawing.Point(504, 151);
+            this.btnMesa10.Name = "btnMesa10";
+            this.btnMesa10.Size = new System.Drawing.Size(79, 80);
+            this.btnMesa10.TabIndex = 9;
+            this.btnMesa10.Text = "M 10";
+            this.btnMesa10.UseVisualStyleBackColor = true;
+            // 
+            // btnMesa11
+            // 
+            this.btnMesa11.Font = new System.Drawing.Font("Calibri", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMesa11.Image = ((System.Drawing.Image)(resources.GetObject("btnMesa11.Image")));
+            this.btnMesa11.Location = new System.Drawing.Point(49, 278);
+            this.btnMesa11.Name = "btnMesa11";
+            this.btnMesa11.Size = new System.Drawing.Size(79, 80);
+            this.btnMesa11.TabIndex = 10;
+            this.btnMesa11.Text = "M 11";
+            this.btnMesa11.UseVisualStyleBackColor = true;
+            // 
+            // btnMesa12
+            // 
+            this.btnMesa12.Font = new System.Drawing.Font("Calibri", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMesa12.Image = ((System.Drawing.Image)(resources.GetObject("btnMesa12.Image")));
+            this.btnMesa12.Location = new System.Drawing.Point(163, 278);
+            this.btnMesa12.Name = "btnMesa12";
+            this.btnMesa12.Size = new System.Drawing.Size(79, 80);
+            this.btnMesa12.TabIndex = 11;
+            this.btnMesa12.Text = "M 12";
+            this.btnMesa12.UseVisualStyleBackColor = true;
+            // 
+            // btnMesa13
+            // 
+            this.btnMesa13.Font = new System.Drawing.Font("Calibri", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMesa13.Image = ((System.Drawing.Image)(resources.GetObject("btnMesa13.Image")));
+            this.btnMesa13.Location = new System.Drawing.Point(276, 278);
+            this.btnMesa13.Name = "btnMesa13";
+            this.btnMesa13.Size = new System.Drawing.Size(79, 80);
+            this.btnMesa13.TabIndex = 12;
+            this.btnMesa13.Text = "M 13";
+            this.btnMesa13.UseVisualStyleBackColor = true;
+            // 
+            // btnMesa14
+            // 
+            this.btnMesa14.Font = new System.Drawing.Font("Calibri", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMesa14.Image = ((System.Drawing.Image)(resources.GetObject("btnMesa14.Image")));
+            this.btnMesa14.Location = new System.Drawing.Point(390, 278);
+            this.btnMesa14.Name = "btnMesa14";
+            this.btnMesa14.Size = new System.Drawing.Size(79, 80);
+            this.btnMesa14.TabIndex = 13;
+            this.btnMesa14.Text = "M 14";
+            this.btnMesa14.UseVisualStyleBackColor = true;
+            // 
+            // btnMesa15
+            // 
+            this.btnMesa15.Font = new System.Drawing.Font("Calibri", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMesa15.Image = ((System.Drawing.Image)(resources.GetObject("btnMesa15.Image")));
+            this.btnMesa15.Location = new System.Drawing.Point(504, 278);
+            this.btnMesa15.Name = "btnMesa15";
+            this.btnMesa15.Size = new System.Drawing.Size(79, 80);
+            this.btnMesa15.TabIndex = 14;
+            this.btnMesa15.Text = "M 15";
+            this.btnMesa15.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Tan;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnMesa15);
+            this.Controls.Add(this.btnMesa14);
+            this.Controls.Add(this.btnMesa13);
+            this.Controls.Add(this.btnMesa12);
+            this.Controls.Add(this.btnMesa11);
+            this.Controls.Add(this.btnMesa10);
+            this.Controls.Add(this.btnMesa9);
+            this.Controls.Add(this.btnMesa8);
+            this.Controls.Add(this.btnMesa7);
+            this.Controls.Add(this.btnMesa6);
+            this.Controls.Add(this.btnMesa5);
+            this.Controls.Add(this.btnMesa4);
+            this.Controls.Add(this.btnMesa3);
+            this.Controls.Add(this.btnMesa2);
+            this.Controls.Add(this.btnMesa1);
+            this.Name = "Form2";
+            this.Text = "Mesas";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMesa1;
+        private System.Windows.Forms.Button btnMesa2;
+        private System.Windows.Forms.Button btnMesa3;
+        private System.Windows.Forms.Button btnMesa4;
+        private System.Windows.Forms.Button btnMesa5;
+        private System.Windows.Forms.Button btnMesa6;
+        private System.Windows.Forms.Button btnMesa7;
+        private System.Windows.Forms.Button btnMesa8;
+        private System.Windows.Forms.Button btnMesa9;
+        private System.Windows.Forms.Button btnMesa10;
+        private System.Windows.Forms.Button btnMesa11;
+        private System.Windows.Forms.Button btnMesa12;
+        private System.Windows.Forms.Button btnMesa13;
+        private System.Windows.Forms.Button btnMesa14;
+        private System.Windows.Forms.Button btnMesa15;
+    }
+}
