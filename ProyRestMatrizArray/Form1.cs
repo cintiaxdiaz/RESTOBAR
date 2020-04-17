97a70a1440c1a9230b74efc8d8e27eed4c1b9f55
cintiaxdiaz <63810271+cintiaxdiaz@users.noreply.github.com>
2020-04-16 22:36:15 -0400

    verificador de rut

--- ProyRestMatrizArray/Form1.cs
+++ ProyRestMatrizArray/Form1.cs
@@ -19,10 +19,74 @@ namespace ProyRestMatrizArray
 
         private void buttonIngresar_Click(object sender, EventArgs e)
         {
-            Form formulario = new Form2();
-            formulario.Show();
+            
 
-            int a = 7;
+            string rut = textBoxUsuario.Text;
+            //rut = rut.Replace(".", string.Empty); ( se lo quitamos??)
+
+            if (rut.Length == 9) {
+                rut = "0" + rut;
+            }
+
+            if (rut[8] != '-') {
+
+               // textBox2.Text = "Incorrecto, falta guión";
+                return;
+            }
+
+
+            int n0, n1, n2, n3, n4, n5, n6, n7 = 0;
+
+            int CONSTANTE0 = 3;
+            int CONSTANTE1 = 2;
+            int CONSTANTE2 = 7;
+            int CONSTANTE3 = 6;
+            int CONSTANTE4 = 5;
+            int CONSTANTE5 = 4;
+            int CONSTANTE6 = 3;
+            int CONSTANTE7 = 2;
+
+            n0 = CONSTANTE0 * Int32.Parse(rut[0].ToString());
+            n1 = CONSTANTE1 * Int32.Parse(rut[1].ToString());
+            n2 = CONSTANTE2 * Int32.Parse(rut[2].ToString());
+            n3 = CONSTANTE3 * Int32.Parse(rut[3].ToString());
+            n4 = CONSTANTE4 * Int32.Parse(rut[4].ToString());
+            n5 = CONSTANTE5 * Int32.Parse(rut[5].ToString());
+            n6 = CONSTANTE6 * Int32.Parse(rut[6].ToString());
+            n7 = CONSTANTE7 * Int32.Parse(rut[7].ToString());
+
+            double suma = n0 + n1 + n2 + n3 + n4 + n5 + n6 + n7;
+            double divisiondecimal = suma / 11;
+            Console.WriteLine(divisiondecimal);
+            double divisionentero = (int)divisiondecimal;
+            Console.WriteLine(divisionentero);
+            double valordecimal = divisiondecimal - divisionentero;
+            double resta11 = 11 - (11 * (valordecimal));
+            resta11 = Math.Round(resta11);
+            int digito = (int)resta11;
+
+
+            if (digito == 11) {
+                digito = 0;
+            }
+
+            int digitoVer;
+
+            if ((rut[9] == 'k') | (rut[9] == 'K')) {
+                digitoVer = 10;
+            } else {
+                digitoVer = Int32.Parse(rut[9].ToString());
+            }
+
+            if (digito == digitoVer) {
+
+                Form formulario = new Form2();
+                formulario.Show(); 
+
+            } else {
+
+                //textBox2.Text = "RUT invalido"; aqui iria el mensaje
+            }
         }
 
        
